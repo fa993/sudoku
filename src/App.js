88cf31b7e7ff2601d576591870e4efbbc4ddf97c
ameyakore <kore.ameya@gmail.com>
2023-02-20 19:44:02 +0530

    feat: table constructed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <SudokuGrid />;
+}
+
+// function SudokuTable({props}) {
+//   const rows = []
+//   for(let i = 0; i < 9; i++) {
+//     rows.push(<SudokuRow key={i} />)
+//   }
+//   return (
+//     <div style={{margin: '0.5rem', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+//       {rows}
+//     </div>
+//   );
+// }
+
+// function SudokuRow({props}) {
+//   const cells = []
+//   for(let i = 0; i < 9; i++) {
+//     cells.push(<SudokuBox key={i} num={i}/>)
+//   }
+//   return (
+//     <div style={{display: 'flex' , justifyContent: 'center'}}>
+//       {cells}
+//     </div>
+//   );
+// }
+
+function isDigit(r) {
+	return (
+		r === '1' ||
+		r === '2' ||
+		r === '3' ||
+		r === '4' ||
+		r === '5' ||
+		r === '6' ||
+		r === '7' ||
+		r === '8' ||
+		r === '9'
+	);
+}
+
+function SudokuBox({ index }) {
+	const [num, setNum] = useState(' ');
+	return (
+		<div
+			className='sudoku-cell sudoku-square'
+			tabIndex={index}
+			onKeyDown={(e) => setNum(isDigit(e.key) ? e.key : ' ')}
+		>
+			<div
+				style={{
+					textAlign: 'center',
+					visibility: isDigit(num) ? 'visible' : 'hidden',
+				}}
+			>
+				{num}
+			</div>
+		</div>
+	);
+}
+
+function SudokuGrid() {
+	const cells = [];
+	for (let i = 0; i < 81; i++) {
+		cells.push(<SudokuBox key={i} index={i} />);
+	}
+	return <div className='sudoku-grid'>{cells}</div>;
 }
 
 export default App;
